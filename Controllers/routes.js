47e34ce0e024fc765efd44ce966e0420e1c6a557
CrisGMO <cristiancampos1508@gmail.com>
2024-11-21 04:43:19 -0300

    nuevas rutas: getById, getCount, deleteById

--- Controllers/routes.js
+++ Controllers/routes.js
@@ -7,8 +7,11 @@ export const createFotoRouter = ({ fotoModel }) => {
   const fotoController = new FotoController({ fotoModel })
   
   fotoRouter.get('/', fotoController.getAll)
+  fotoRouter.get('/count', fotoController.getAll)
   fotoRouter.post('/', fotoController.create)
   fotoRouter.delete('/deleteAll', fotoController.deleteAll)
+  fotoRouter.delete('/delete/:id', fotoController.deleteById)
+  fotoRouter.get('/:id', fotoController.getById)
 
   return fotoRouter
 }
